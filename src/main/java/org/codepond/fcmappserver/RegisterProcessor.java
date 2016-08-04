426a268d43f1db5b47e197d6d0df4f080d38f9f3
Nimrod Dayan <dev@codepond.org>
2016-08-04 18:47:12 +0300

    Modified CcsClient to use FCM insead of GCM

---
+++ src/main/java/org/codepond/fcmappserver/RegisterProcessor.java
@@ -0,0 +1,34 @@
+/*
+ * Modifications Copyright 2016 Nimrod Dayan
+ *
+ * Copyright 2014 Wolfram Rittmeyer.
+ *
+ * Portions Copyright Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.codepond.fcmappserver;
+
+/**
+ * Handles a user registration.
+ */
+public class RegisterProcessor implements PayloadProcessor{
+
+    @Override
+    public void handleMessage(CcsMessage msg) {
+        String accountName = msg.getPayload().get("account");
+        PseudoDao.getInstance().addRegistration(msg.getFrom(), accountName);
+    }
+
+}
